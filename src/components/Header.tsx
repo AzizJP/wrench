235f7ee291d4b221e3d0b42c0995b183e7b27f5b
Aziz <t1-zer@yandex.ru>
2022-10-27 21:50:24 +0300

    feat: finish Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,18 @@ import styled from '@emotion/styled';
 import {FC, memo} from 'react';
 
 import {ReactComponent as Logo} from '../assets/logo.svg';
+import {ReactComponent as PersonIcon} from '../assets/person-icon.svg';
 
 const HeaderWrapper = styled.div`
   display: flex;
   align-items: center;
-  background: #add8e6;
+  justify-content: space-between;
+  background: #4f27bf;
+  width: 100%;
 `;
 
 const HeaderTitle = styled.h1`
+  margin: 0;
   font-weight: 700;
   font-size: 20px;
   line-height: 24px;
@@ -17,19 +21,50 @@ const HeaderTitle = styled.h1`
 `;
 
 const LogoWrapper = styled.span`
+  margin: 16px 24px 16px 32px;
   & > svg {
     height: 48px;
     width: 48px;
   }
 `;
 
+const HeaderItemsContainer = styled.div`
+  display: flex;
+  margin: 0;
+  align-items: center;
+`;
+
+const IconWrapper = styled.div`
+  margin: 16px;
+  & > svg {
+    height: 48px;
+    width: 48px;
+  }
+`;
+
+const PersonTitle = styled.h2`
+  margin: 28px 32px 28px 0;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+  color: #ffffff;
+`;
+
 const Header: FC = memo(() => {
   return (
     <HeaderWrapper>
-      <LogoWrapper>
-        <Logo />
-      </LogoWrapper>
-      <HeaderTitle>Helo</HeaderTitle>
+      <HeaderItemsContainer>
+        <LogoWrapper>
+          <Logo />
+        </LogoWrapper>
+        <HeaderTitle>Wrench CRM</HeaderTitle>
+      </HeaderItemsContainer>
+      <HeaderItemsContainer>
+        <IconWrapper>
+          <PersonIcon />
+        </IconWrapper>
+        <PersonTitle>Имя Фамилия</PersonTitle>
+      </HeaderItemsContainer>
     </HeaderWrapper>
   );
 });
