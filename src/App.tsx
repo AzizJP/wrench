8c207070c206dce83d0fdfddd1615a9878aa321f
Aziz <t1-zer@yandex.ru>
2022-10-28 11:12:15 +0300

    fixed: add changes to assembly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import styled from '@emotion/styled';
 import {FC, memo} from 'react';
 
 import Header from './components/Header';
 
+const Page = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  margin: 0;
+`;
+
 const App: FC = memo(() => {
-  return <Header />;
+  return (
+    <Page>
+      <Header />
+    </Page>
+  );
 });
 
 export default App;
