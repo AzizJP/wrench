80b00b008239edddf376e2290542b8db0ba50c38
Aziz <t1-zer@yandex.ru>
2022-10-28 16:44:46 +0300

    feat: adaptive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,51 @@ import {FC, memo} from 'react';
 import {ReactComponent as Logo} from '../assets/logo.svg';
 import {ReactComponent as PersonIcon} from '../assets/person-icon.svg';
 
+const breakpoints: Array<number> = [420, 768];
+const mq: Array<string> = breakpoints.map(bp => `@media (min-width: ${bp}px)`);
+
 const HeaderWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   background: #4f27bf;
-  padding: 16px 32px;
+  padding: 8px 12px;
+  ${mq[0]} {
+    padding: 12px 24px;
+  }
+  ${mq[1]} {
+    padding: 16px 32px;
+  }
 `;
 
-const HeaderTitle = styled.h1`
+const HeaderTitle = styled.h2`
   margin: 0;
   font-weight: 700;
-  font-size: 20px;
-  line-height: 24px;
+  font-size: 12px;
+  line-height: 16px;
   color: #ffffff;
+  ${mq[0]} {
+    font-size: 16px;
+    line-height: 20px;
+  }
+  ${mq[1]} {
+    font-size: 20px;
+    line-height: 24px;
+  }
 `;
 
-const LogoWrapper = styled.span`
+const IconWrapper = styled.span`
+  height: 24px;
+  width: 24px;
   & > svg {
+    height: 100%;
+    width: 100%;
+  }
+  ${mq[0]} {
+    height: 36px;
+    width: 36px;
+  }
+  ${mq[1]} {
     height: 48px;
     width: 48px;
   }
@@ -33,35 +60,20 @@ const HeaderItemsContainer = styled.div<{gap: number}>`
   align-items: center;
 `;
 
-const IconWrapper = styled.span`
-  & > svg {
-    height: 48px;
-    width: 48px;
-  }
-`;
-
-const PersonTitle = styled.h2`
-  margin: 0;
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 24px;
-  color: #ffffff;
-`;
-
 const Header: FC = memo(() => {
   return (
     <HeaderWrapper>
       <HeaderItemsContainer gap={24}>
-        <LogoWrapper>
+        <IconWrapper>
           <Logo />
-        </LogoWrapper>
+        </IconWrapper>
         <HeaderTitle>Wrench CRM</HeaderTitle>
       </HeaderItemsContainer>
       <HeaderItemsContainer gap={16}>
         <IconWrapper>
           <PersonIcon />
         </IconWrapper>
-        <PersonTitle>Имя Фамилия</PersonTitle>
+        <HeaderTitle>Имя Фамилия</HeaderTitle>
       </HeaderItemsContainer>
     </HeaderWrapper>
   );
