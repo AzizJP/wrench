d1aa0b62654ce07394bb803dad3f67421ff8ee9e
Aziz <t1-zer@yandex.ru>
2022-10-29 12:56:50 +0300

    feat: add global styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import {Global, css} from '@emotion/react';
 import styled from '@emotion/styled';
 import {FC, memo} from 'react';
 
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 
+const globalStyle = css`
+  body {
+    margin: 0;
+  }
+`;
 const Page = styled.div`
   width: 100%;
   min-height: 100vh;
@@ -12,6 +18,7 @@ const Page = styled.div`
 const App: FC = memo(() => {
   return (
     <Page>
+      <Global styles={globalStyle} />
       <Header />
       <Sidebar />
     </Page>
